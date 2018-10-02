c0d6bc901ecd6ca7120b5394e0a7e49a0e61c74e
sublun <sublun@126.com>
2018-10-02 20:52:09 +0800

    efds

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -2,5 +2,6 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("小猪佩奇");
+        System.out.println("小兔子乖乖");
     }
 }
